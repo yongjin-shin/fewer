5a83fb22b0bf61a2b7e7bea563dc3db6c054a617
GihunLee <opcrisis@kaist.ac.kr>
2020-05-06 19:13:55 +0900

    pruning handler (not debugged)

--- pruning/PruningHandler.py
+++ pruning/PruningHandler.py
@@ -2,19 +2,28 @@ import torch
 import torch.nn as nn
 import torch.nn.utils.prune as prune
 
+from pruning_utils import *
+
 __all__ = ['PruningHandler']
 
-class PruningHandler():    
-    def __init__(self, pruning_plan, recovery_plan, recovery_method=None, prune_method=None, globaly=False):
-        self.pruning_plan = pruning_plan
-        self.recovery_plan = recovery_plan
+
+class PruningHandler():
+    def __init__(self, args, prune_method=None, recovery_method=None, globaly=True):
+        # Specify pruning & recovery plan for each round
+        self.pruning_plan = self._planner(args.pruning_plan)
+        self.recovery_plan = self._planner(args.recovery_plan)
         
+        # Define pruning targets
+        self.weight_set = self._global_setter(model)
+        
+        # Set pruning options
+        self.globaly = globaly
         self.pruning = prune.l1_unstructured if prune_method is None else prune_method
         self.recovery = recovery_method
-        self.globaly = globaly
         
         
-    def pruner(self, server_model, fed_round):
+    def pruner(self, model, fed_round):
+        """Prune weights (expected to be called before distribution)"""
         if self.globaly:
             weight_set = _global_setter(server_model)
             prune.global_unstructured(weight_set, pruning_method=self.pruning, amount=pruning_plan['global'][fed_round])
@@ -26,8 +35,8 @@ class PruningHandler():
                 self.pruning(module, name='weight', amount=pruning_plan['fc'][fed_round])
                 
         
-    def recovery(self, local_model, fed_round):
-        """"""
+    def recoverer(self, model, recovery_signals, fed_round):
+        """Recover weights (expected to be called before aggregation)"""
         recovery_signal = []
         for name, module in local_model.named_modules():
             if isinstance(module, torch.nn.Conv2d):
@@ -40,12 +49,37 @@ class PruningHandler():
         return recovery_signal
 
     
-    def _global_setter(self, model):
+    def global_sparsity_evaluator():
+        """Evaluate current sparsity of model"""
+        num_sparse, num_weight = 0, 0
+
+        for elem in self.weight_set:
+            num_sparse += (elem[0].weight == 0).sum().item()
+            num_weight += elem[0].weight.nelement()
+
+        sparsity = round(num_sparse/num_weight, 4)
+
+        return sparsity
+    
+    
+    def _global_setter(model):
+        """Specify target weight_set to prune"""
         modules = model.__dict__['_modules'].keys()
-        conv_set, fc_set = [], []
+        weight_set = []
+
         for module in modules:
             if ('conv' in module) or ('fc' in module):
-                conv_set.append((getattr(model, module), 'weight'))        
-        
+                module_weight = (getattr(model, module), 'weight')
+                weight_set.append(module_weight)
+
+        weight_set = tuple(weight_set)
+
         return weight_set
+
+    def _planner(plan, mode='pruning'):
+        if mode == 'pruning':
+            pass
+        else mode == 'recovery':
+            pass
         
+        return plan
