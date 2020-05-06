5ca9797ec48fd4ab9acb588750157d24084f06a8
GihunLee <opcrisis@kaist.ac.kr>
2020-05-06 19:58:12 +0900

    PruningHandler refine

--- pruning/PruningHandler.py
+++ pruning/PruningHandler.py
@@ -9,12 +9,13 @@ __all__ = ['PruningHandler']
 
 class PruningHandler():
     def __init__(self, args, prune_method=None, recovery_method=None, globaly=True):
+        """
+        Pruning Handler to control pruning & recovery
+        (default setting) : globally prune with L1 norm & no recovery
+        """
         # Specify pruning & recovery plan for each round
         self.pruning_plan = self._planner(args.pruning_plan)
         self.recovery_plan = self._planner(args.recovery_plan)
-        
-        # Define pruning targets
-        self.weight_set = self._global_setter(model)
         
         # Set pruning options
         self.globaly = globaly
@@ -24,43 +25,39 @@ class PruningHandler():
         
     def pruner(self, model, fed_round):
         """Prune weights (expected to be called before distribution)"""
-        if self.globaly:
-            weight_set = _global_setter(server_model)
-            prune.global_unstructured(weight_set, pruning_method=self.pruning, amount=pruning_plan['global'][fed_round])
-        
-        for name, module in server_model.named_modules():
-            if isinstance(module, torch.nn.Conv2d):
-                self.pruning(module, name='weight', amount=pruning_plan['conv'][fed_round])
-            if isinstance(module, torch.nn.Linear):
-                self.pruning(module, name='weight', amount=pruning_plan['fc'][fed_round])
-                
-        
-    def recoverer(self, model, recovery_signals, fed_round):
-        """Recover weights (expected to be called before aggregation)"""
-        recovery_signal = []
-        for name, module in local_model.named_modules():
-            if isinstance(module, torch.nn.Conv2d):
-                signal = self.recovery(module, amount=recovery_plan['conv'][fed_round])
-            if isinstance(module, torch.nn.Linear):
-                signal = self.recovery(module, amount=recovery_plan['fc'][fed_round]) 
+            weight_set = self._global_setter(model)
             
-            recovery_signal.append((name, signal))
+            prune.global_unstructured(weight_set,
+                                      pruning_method=self.pruning, 
+                                      amount=self.pruning_plan[fed_round])
+        
+        
+        #for name, module in server_model.named_modules():
+        #    if isinstance(module, torch.nn.Conv2d):
+        #        self.pruning(module, name='weight', amount=pruning_plan['conv'][fed_round])
+        #    if isinstance(module, torch.nn.Linear):
+        #        self.pruning(module, name='weight', amount=pruning_plan['fc'][fed_round])
+                
+    def recoverer(self, model, recovery_signals, fed_round):
+        """
+        To be implemented
+        Recover weights (expected to be called before aggregation)
+        """
         
         return recovery_signal
 
-    
-    def global_sparsity_evaluator():
+    def global_sparsity_evaluator(model):
         """Evaluate current sparsity of model"""
         num_sparse, num_weight = 0, 0
-
-        for elem in self.weight_set:
+        weight_set = self._global_setter(model)
+        
+        for elem in weight_set:
             num_sparse += (elem[0].weight == 0).sum().item()
             num_weight += elem[0].weight.nelement()
 
         sparsity = round(num_sparse/num_weight, 4)
 
         return sparsity
-    
     
     def _global_setter(model):
         """Specify target weight_set to prune"""
