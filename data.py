2046142efd08ca953f7f2caa7a665fdfd4e781ac
tmdwo0910 <tmdwo0910@kaist.ac.kr>
2020-05-19 14:38:44 +0900

    recovery draft by SJ

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+
 from pathlib import Path
 import torch
 from torchvision import datasets
--- local.py
+++ local.py
@@ -1,8 +1,9 @@
+
 import copy
 import torch
 from data import Mydataset
 from torch.utils.data import DataLoader
-
+from networks import MLP, MnistCNN, CifarCnn, TestCNN
 
 class Local:
     def __init__(self, args, c_id):
@@ -36,12 +37,43 @@ class Local:
                 train_loss += loss
 
                 self.model.zero_grad()
+
                 loss.backward()
+
                 self.optim.step()
 
             # print(f"{self.c_id}th Client | Train loss: {train_loss / (itr + 1):.3f} | Train Acc: {correct * 100 / len(self.dataset['y']):.3f}@ {ep}")
         return train_loss / ((ep+1) * (itr+1))
-    
+
+
+    def train_with_recovery(self):
+        train_loss, itr, ep = 0, 0, 0
+        for ep in range(self.args.local_ep):
+            # correct = 0
+            for itr, (x, y) in enumerate(self.data_loader):
+                logprobs = self.model(x)
+                temp_probs = self.model_dense(x)
+                loss = self.loss_func(logprobs, y)
+
+                train_loss += loss
+
+                self.model.zero_grad()
+                self.model_dense.zero_grad()
+                loss.backward()
+
+                # print('start')
+
+                for mask_param, dense_param in zip(self.model.parameters(),self.model_dense.parameters()):
+                    print('============================')
+                    print(dense_param.grad)
+                    dense_param.grad = mask_param.grad
+                    print(dense_param.grad)
+                    print('!!!!!!!!!!!!!!!!!!!!!!!!!')
+                # self.optim.step()
+                self.optim_dense.step()
+
+        return train_loss / ((ep+1) * (itr+1))
+
     def get_dataset(self, dataset):
         self.dataset = dataset
         self.data_loader = DataLoader(Mydataset(self.dataset, self.args), batch_size=self.args.local_bs, shuffle=True)
@@ -51,20 +83,69 @@ class Local:
     def set_unique(self, nb_unique):
         self.nb_unique_label = nb_unique
 
-    def get_model(self, model):
+    def get_model(self, model,model_name,model_reference):
         self.model = copy.deepcopy(model)
+
+        import pickle
+        self.model_dense = copy.deepcopy(pickle.loads(pickle.dumps(model)))
+
+        # self.model_dense = copy.deepcopy(model_reference)
+        #
+        # params1 = self.model.named_parameters()
+        # params2 = self.model_dense.named_parameters()
+        #
+        #
+        # import pickle
+        #
+        # for name1, param1 in params1:
+        #     print(name1)
+        #     for name2, param2 in params2:
+        #         print('==========')
+        #         print(name1)
+        #         print(name2)
+                # if name1 == name2:
+
+                    # param2.data.copy_(params)
+
+
+        # # model.attribute = list(model.attribute)
+        # self.model_dense  = copy.deepcopy(model_reference)
+        #
+
+        # dict_params2 = dict(params2)
+        #
+        # for name1, param1 in params1:
+        #     if name1 in dict_params2:
+        #         # print(name1)
+        #         dict_params2[name1].data.copy_(param1.data)
+
+
+        # self.model_dense= copy.deepcopy(model_reference)
+        # print(self.model.state_dict())
+        # self.model_dense.load_state_dict(torch.from_numpy(self.model.state_dict().cpu().numpy()))
+        # self.model_dense.load_state_dict(dict_params2)
+
+
         if 'sgd' == self.args.optimizer:
             self.optim = torch.optim.SGD(self.model.parameters(),
                                          lr=self.args.lr,
                                          momentum=self.args.momentum,
                                          weight_decay=self.args.weight_decay)
+            self.optim_dense = torch.optim.SGD(self.model_dense.parameters(),
+                                         lr=self.args.lr,
+                                         momentum=self.args.momentum,
+                                         weight_decay=self.args.weight_decay)
         elif 'adam' == self.args.optimizer:
             self.optim = torch.optim.Adam(self.model.parameters(), lr=self.args.lr)
+            self.optim_dense = torch.optim.Adam(self.model_dense.parameters(), lr=self.args.lr)
         else:
             raise NotImplementedError
 
     def upload_model(self):
         return copy.deepcopy(self.model.state_dict())
+
+    def upload_recovery_model(self):
+        return copy.deepcopy(self.model_dense.state_dict())
     
     def upload_recovery_signal(self):
         return copy.deepcopy(self.recovery)
