c78c0048bfaf5ebcd0f00d744a83a0c2aa448d2a
GihunLee <opcrisis@kaist.ac.kr>
2020-08-05 20:03:23 +0900

    structures

--- networks.py
+++ networks.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 import torch
 
 from resnet_cifar import *
+from vgg_cifar import *
 
 
 def create_nets(args, location):
@@ -17,15 +18,23 @@ def create_nets(args, location):
     if args.model == 'mlp':
         model = MLP(784, args.hidden, 10)
     elif args.model == 'deep_mlp':
-        model = DeepMLP(784, args.hidden, 10)
+        model = DeepMLP(3072, args.hidden, 10)
     elif args.model == 'mnistcnn':
         model = MnistCNN(1, 10)
     elif args.model == 'cifarcnn':
         model = CifarCnn(3, 10)
+    elif args.model == 'cifarcnnlarge':
+        model = CifarCnnLarge(3, 10)
+    elif args.model == 'cifarcnnslim':
+        model = CifarCnnSlim(3, 10)
     elif args.model == 'testcnn':
         model = TestCNN(_in_dim, 10)
-    elif args.model == 'vgg':
-        model = VGG(_in_dim, 10)
+    elif args.model == 'vgg11':
+        model = vgg11()
+    elif args.model == 'vgg11_original':
+        model = vgg11(m_type='original')
+    elif args.model == 'vgg11_slim':
+        model = vgg11(m_type='slim')
     elif args.model == 'res8':
         model = resnet8(num_classes=10)
     else:
@@ -121,7 +130,48 @@ class CifarCnn(nn.Module):
         x = self.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+    
+class CifarCnnSlim(nn.Module):
+    def __init__(self, dim_in, dim_out):
+        super(CifarCnnSlim, self).__init__()
+        self.conv1 = nn.Conv2d(dim_in, 16, 5)
+        self.conv2 = nn.Conv2d(16, 32, 5)
+        self.fc1 = nn.Linear(32*5*5, 256)
+        self.fc2 = nn.Linear(256, 64)
+        self.fc3 = nn.Linear(64, dim_out)
+        self.relu = nn.ReLU()
+        self.mp = nn.MaxPool2d(2)
+        print("CifarCnn was made")
 
+    def forward(self, x):
+        x = self.mp(self.relu(self.conv1(x)))
+        x = self.mp(self.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
+class CifarCnnLarge(nn.Module):
+    def __init__(self, dim_in, dim_out):
+        super(CifarCnnLarge, self).__init__()
+        self.conv1 = nn.Conv2d(dim_in, 64, 5)
+        self.conv2 = nn.Conv2d(64, 128, 5)
+        self.fc1 = nn.Linear(128*5*5, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, dim_out)
+        self.relu = nn.ReLU()
+        self.mp = nn.MaxPool2d(2)
+        print("CifarCnn was made")
+
+    def forward(self, x):
+        x = self.mp(self.relu(self.conv1(x)))
+        x = self.mp(self.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 # Testing model for pruning operations
 class TestCNN(nn.Module):
@@ -146,98 +196,4 @@ class TestCNN(nn.Module):
         x = self.globalpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
-        return x
-
-
-# VGG 11-layer model
-class VGG(nn.Module):
-    def __init__(self, dim_in=3, dim_out=10, init_weights=True):
-        if dim_in == 1:
-            raise NotImplemented
-
-        super(VGG, self).__init__()
-        self.conv1 = nn.Conv2d(dim_in, 32, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-        self.conv5 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-        self.conv6 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-        self.conv7 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-        self.conv8 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
-        self.relu = nn.ReLU()
-
-        self.fc1 = nn.Linear(in_features=128, out_features=128, bias=True)
-        self.fc2 = nn.Linear(in_features=128, out_features=128, bias=True)
-        self.fc3 = nn.Linear(in_features=128, out_features=dim_out, bias=True)
-
-        if init_weights:
-            self._initialize_weights()
-
-        print("VGG was made")
-
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0, 0.01)
-                nn.init.constant_(m.bias, 0)
-
-    def forward(self, x):
-        #       (0): Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-        #       (1): ReLU(inplace=True)
-        #       (2): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
-        x = self.maxpool(self.relu(self.conv1(x)))
-
-        #       (3): Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-        #       (4): ReLU(inplace=True)
-        #       (5): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
-        x = self.maxpool(self.relu(self.conv2(x)))
-
-        #       (6): Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-        #       (7): ReLU(inplace=True)
-        x = self.relu(self.conv3(x))
-
-        #       (8): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-        #       (9): ReLU(inplace=True)
-        #       (10): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
-        x = self.maxpool(self.relu(self.conv4(x)))
-
-        #       (11): Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-        #       (12): ReLU(inplace=True)
-        x = self.relu(self.conv5(x))
-
-        #       (13): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-        #       (14): ReLU(inplace=True)
-        #       (15): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
-        x = self.maxpool(self.relu(self.conv6(x)))
-
-        #       (16): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-        #       (17): ReLU(inplace=True)
-        x = self.relu(self.conv7(x))
-
-        #       (18): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-        #       (19): ReLU(inplace=True)
-        #       (20): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
-        x = self.maxpool(self.relu(self.conv8(x)))
-        x = torch.flatten(x, 1)
-
-        #       (0): Linear(in_features=25088, out_features=4096, bias=True)
-        #       (1): ReLU(inplace=True)
-        #       (2): Dropout(p=0.5, inplace=False)
-        x = self.relu(self.fc1(x))
-
-        #       (3): Linear(in_features=4096, out_features=4096, bias=True)
-        #       (4): ReLU(inplace=True)
-        #       (5): Dropout(p=0.5, inplace=False)
-        x = self.relu(self.fc2(x))
-
-        #       (6): Linear(in_features=4096, out_features=10, bias=True)
-        x = self.fc3(x)
         return x
